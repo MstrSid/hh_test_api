be5a1febea095c5d5d2739d94411bab3f58be45c
Eugene Kos <mstrsid@live.com>
2022-08-11 08:01:48 +0300

    refactor

--- apiQuery.js
+++ apiQuery.js
@@ -6,24 +6,26 @@ const div = document.createElement('div');
 fetch(`${baseUrl}/vacancies?professional_role=96`)
 	.then(response => response.json())
 	.then(data => Object.values(data))
-	.then(res => {
-		console.log(res[0][0]);
-		res[0].forEach(item => {
-			div.innerHTML += `
+	.then(res => showData(res));
+
+function showData(data) {
+	console.log(data[0][0]);
+	data[0].forEach(item => {
+		div.innerHTML += `
 ${item['name']}
 <ul>Дата: ${item['published_at'].substring(0, 10)}</ul>
 <ul>Город: ${item['address'] ? item['address']['city'] : 'нет данных'}</ul>
 <ul>Работодатель: ${item['employer'] ? item['employer']['name'] : 'нет данных'}</ul>
 <ul>Зарплата, от ${item['salary'] ?
-				item['salary']['from'] + item['salary']['currency'] :
-				'нет данных'} до ${item['salary'] ?
-				item['salary']['to'] ?
-					item['salary']['to'] + item['salary']['currency'] :
-					'нет данных' :
-				'нет данных'}</ul>
+			item['salary']['from'] + item['salary']['currency'] :
+			'нет данных'} до ${item['salary'] ?
+			item['salary']['to'] ?
+				item['salary']['to'] + item['salary']['currency'] :
+				'нет данных' :
+			'нет данных'}</ul>
 
 
 <br><br>`
-		});
-		document.querySelector('body').append(div);
 	});
+	document.querySelector('body').append(div);
+}
